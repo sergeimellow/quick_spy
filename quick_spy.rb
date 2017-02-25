f0132326bd820fefe1c492c7d41c9861432ee428
Colin Fike <colin.fike@gmail.com>
2017-02-25 01:46:50 -0500

    Lots of fun left to figure out with threading

--- quick_spy.rb
+++ quick_spy.rb
@@ -4,14 +4,24 @@ require 'uri'
 
 class QuickSpy
   def initialize
-    @site_queue = []
+    @init_time = Time.now
+    @site_queue = ['www.reddit.com']
     @hosts_lists = []
     @affected_counter = 0
     @request_counter = 0
+    @timeout_counter = 0
   end
 
-  def start_scraping(url)
-    @site_queue << url
+  def report
+    while true
+      sleep 10
+      print_report
+    end
+  end
+
+  def start_scraping()
+  # def start_scraping(url)
+    # @site_queue << url
     while @site_queue.any?
       url = @site_queue.shift
       puts "Checking #{get_host_without_www(url)}..."
@@ -20,12 +30,13 @@ class QuickSpy
         @affected_counter += 1
       end
       begin
-        page = Nokogiri::HTML(RestClient::Request.execute(:method => :get, :url => url, :timeout => 10, :open_timeout => 10, :user_agent => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.35"))
+        page = Nokogiri::HTML(RestClient::Request.execute(:method => :get, :url => url, :timeout => 10, :open_timeout => 10, :user_agent => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.84 Safari/537.35"))
         parse_anchors(page)
         @request_counter += 1
-        print_report if @request_counter % 10 == 0
+        # print_report if @request_counter % 10 == 0
       rescue => e
-        puts "Exception: #{e}"
+        @timeout_counter += 1
+        # puts "Exception: #{e}"
       end
     end
   end
@@ -54,14 +65,30 @@ class QuickSpy
   def print_report
     puts ''
     puts '---------------- Progress Report Start ----------------'
+    puts "#{calculate_rpm} requests per minute."
     puts "#{@request_counter} sites tested."
     puts "#{@site_queue.count} sites left in queue."
     puts "#{@hosts_lists.count} unique domains found."
+    puts "#{@timeout_counter} timeouts occured."
+    puts "#{((@timeout_counter.to_f / @request_counter) * 100).round(2)}% of requests timeout."
     puts "#{@affected_counter} potentially affected sites found."
     puts "#{((@affected_counter.to_f / @request_counter) * 100).round(2)}% of sites affected."
     puts '----------------  Progress Report End  ----------------'
     puts ''
   end
+
+  def calculate_rpm
+    elapsed_minutes = (Time.now - @init_time) / 60
+    (@request_counter / elapsed_minutes).round
+  end
 end
 
-QuickSpy.new.start_scraping('reddit.com')
+spider = QuickSpy.new
+# Thread.new{ spider.start_scraping('reddit.com') }
+# Thread.new{ spider.start_scraping('forbes.com') }
+# Thread.new{ spider.start_scraping('ign.com') }
+# Thread.new{ spider.start_scraping('imgur.com') }
+# Thread.new{ spider.start_scraping('namesearch.com') }
+# spider.report
+
+# 1 thread around 20-30 rpm
