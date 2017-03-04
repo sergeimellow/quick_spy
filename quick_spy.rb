a783e0b2e5b509bf65fb248f622abee1e6b77e87
Sergei Mellow <sergei.mellow@gmail.com>
2017-03-04 01:20:08 -0500

    Update quick_spy.rb
    n00b

--- quick_spy.rb
+++ quick_spy.rb
@@ -2,15 +2,12 @@ require 'nokogiri'
 require 'restclient'
 require 'uri'
 require './spider'
-
 class QuickSpy
   def initialize
     @affected_counter = 0
     @spider = Spider.new(self)
   end
-
   # TODO: Move parser functionality here
-
   def start
     @spider.start_spider
   end
